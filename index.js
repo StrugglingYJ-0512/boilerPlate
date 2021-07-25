5ca014e55086803e00f6f7023c3a4e558215a381
username <StrugglingYJ-0512.name>
2021-07-25 21:53:40 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://YJ:dbswn7413@cluster0.kvhlq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+  // 이것들이 안 쓰면 에러가 뜰 수 있음. 
+}).then(() => console.log('MongoClient connected... '))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
